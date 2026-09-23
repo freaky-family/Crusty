0ee2d58642d50179fded590bf8c3a190154a47bd
Esteban Hazanas <esteban.hazanas@epitech.eu>
2026-09-23 18:47:20 +0200

    imp: set pos function

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,7 +9,6 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp_hal::peripherals;
 use esp_hal::{clock::CpuClock};
 use esp_hal::rng::Rng;
 use esp_hal::timer::timg::TimerGroup;
--- src/led.rs
+++ src/led.rs
@@ -46,12 +46,10 @@ pub async fn led_task(mut servos: Servos, ledc: Ledc<'static>) {
         })
         .unwrap();
 
-    // LF Leg
     let mut cl: [channel::Channel<'_, HighSpeed>; 4] = [ledc.channel(channel::Number::Channel1, servos.servo2.reborrow()), ledc.channel(channel::Number::Channel5, servos.servo4.reborrow()),
         ledc.channel(channel::Number::Channel0, servos.servo1.reborrow()), ledc.channel(channel::Number::Channel4, servos.servo3.reborrow())];
     let mut cr: [channel::Channel<'_, HighSpeed>; 4] = [ledc.channel(channel::Number::Channel3, servos.servo6.reborrow()), ledc.channel(channel::Number::Channel7, servos.servo8.reborrow()),
     ledc.channel(channel::Number::Channel2, servos.servo5.reborrow()), ledc.channel(channel::Number::Channel6, servos.servo7.reborrow())];
-    // cl[ID3] = ledc.channel(channel::Number::Channel0, servos.servo1.reborrow());
     for i in 0..4 {
         cl[i].configure(channel::config::Config {
             timer: &hstimer0,
@@ -83,60 +81,66 @@ pub async fn led_task(mut servos: Servos, ledc: Ledc<'static>) {
     let mut duty_info = DutyInfo {
         min_duty:min_duty, duty_gap: duty_gap, duty: 0
     };
-    let mut is_high: u8;
+    let mut is_high: u8 = 0;
     let mut old_state: u8 = 1;
-    let mut state: u8;
     loop {
-        state = LED_STATE.load(Ordering::Relaxed);
-        if state == 0 {
-            is_high = 0;
-            if is_high != old_state {
-                old_state = is_high;
-                println!("STOP");
-                stop(& mut cl, & mut cr, & mut duty_info);
-            }
-        } else if state == 1 {
-            is_high = 1;
-            if is_high != old_state {
-                old_state = is_high;
-                println!("WALK");
-                stop(& mut cl, & mut cr, & mut duty_info);
-            }
-            walk(& mut cl, & mut cr, &mut duty_info, &delay);
-        } else if state == 2 {
-            is_high = 2;
-            if is_high != old_state {
-                println!("HELLO");
-                old_state = is_high;
-                stop(& mut cl, & mut cr, & mut duty_info);
-            }
-            hello(& mut cl, & mut cr, & mut duty_info, &delay);
-        } else if state == 3 {
-            is_high = 3;
-            if is_high != old_state {
-                println!("TURN RIGHT");
-                old_state = is_high;
-                stop(& mut cl, & mut cr, & mut duty_info);
-            }
-            turn_right(& mut cl, & mut cr, &mut duty_info, &delay);
-        } else if state == 4 {
-            is_high = 4;
-            if is_high != old_state {
-                println!("TURN RIGHT");
-                old_state = is_high;
-                stop(& mut cl, & mut cr, & mut duty_info);
-            }
-            turn_left(& mut cl, & mut cr, &mut duty_info, &delay);
-        } else if state == 5 {
-            is_high = 4;
-            if is_high != old_state {
-                println!("freaky");
-                old_state = is_high;
-                stop(& mut cl, & mut cr, & mut duty_info);
-            }
-            freaky(& mut cl, & mut cr, &mut duty_info, &delay);
-        }
+        set_position(&mut cl, &mut cr, &mut duty_info, &delay, &mut is_high, &mut old_state);
         Timer::after(Duration::from_millis(50)).await;
+    }
+}
+
+fn set_position(cl: & mut [channel::Channel<'_, HighSpeed>; 4],
+    cr: & mut [channel::Channel<'_, HighSpeed>; 4],
+    duty_info: & mut DutyInfo, delay: &Delay, is_high: & mut u8,
+    old_state: & mut u8) {
+    let state = LED_STATE.load(Ordering::Relaxed);
+    if state == 0 {
+        *is_high = 0;
+        if is_high != old_state {
+            *old_state = *is_high;
+            println!("STOP");
+            stop(cl, cr, duty_info);
+        }
+    } else if state == 1 {
+        *is_high = 1;
+        if is_high != old_state {
+            *old_state = *is_high;
+            println!("WALK");
+            stop(cl, cr, duty_info);
+        }
+        walk(cl, cr, duty_info, delay);
+    } else if state == 2 {
+        *is_high = 2;
+        if is_high != old_state {
+            println!("HELLO");
+            *old_state = *is_high;
+            stop(cl, cr, duty_info);
+        }
+        hello(cl, cr, duty_info, &delay);
+    } else if state == 3 {
+        *is_high = 3;
+        if is_high != old_state {
+            println!("TURN RIGHT");
+            *old_state = *is_high;
+            stop(cl, cr, duty_info);
+        }
+        turn_right(cl, cr, duty_info, delay);
+    } else if state == 4 {
+        *is_high = 4;
+        if is_high != old_state {
+            println!("TURN RIGHT");
+            *old_state = *is_high;
+            stop(cl, cr, duty_info);
+        }
+        turn_left(cl, cr, duty_info, delay);
+    } else if state == 5 {
+        *is_high = 4;
+        if is_high != old_state {
+            println!("freaky");
+            *old_state = *is_high;
+            stop(cl, cr, duty_info);
+        }
+        freaky(cl, cr, duty_info, delay);
     }
 }
 
